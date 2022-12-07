95c47b8624419db8cb89557d8698c26cafe187be
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-12-07 20:42:43 +0000

    Casi finalizado

--- src/app.py
+++ src/app.py
@@ -34,9 +34,36 @@ def handle_hello():
         "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/member/<int:member_id', methods=['GET'])
+def get_member(member_id):
+    try:        
+        jackson_family.get_member(member_id)
+    except Exception as e:
+        print (e)
+        return jsonify ({"Error", e}), 400
+    return jsonify({"message": "Success"}), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    try:
+        member = request.json()
+        jackson_family.add_member(member)
+    except Exception as e:
+        print (e)
+        return jsonify ({"Error", e}), 400
+    return jsonify({"message": "Success"}), 200
+
+@app.route('/member/<int:member_id', methods=['DELETE'])
+def delete_member(member_id):
+    try:        
+        jackson_family.delete_member(member_id)
+    except Exception as e:
+        print (e)
+        return jsonify ({"Error", e}), 400
+    return jsonify({"message": "Success"}), 200
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -21,14 +21,20 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member['id'] = self._generateId()
+        self.members.append(member)
+        
 
     def delete_member(self, id):
         # fill this method and update the return
+        for member in self._members:
+            if id == member['id']:
+                array.remove(id)
         pass
 
     def get_member(self, id):
         # fill this method and update the return
+        return self._members
         pass
 
     # this method is done, it returns a list with all the family members
